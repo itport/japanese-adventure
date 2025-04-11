7f9b61eee6a6364a5f4b1662e1fad3f19cf8f60f
itport <contact@itport.ro>
2025-04-11 20:32:34 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GameConsole from './components/GameConsole'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="game-title">Japanese Adventure</h1>
+      <GameConsole />
+    </div>
+  )
+}
+
+export default App
